ce19c021551771849b5344e545ede6eb7c0b6045
Andrea Constantinof <andrea.tinof@gmail.com>
2019-02-03 14:46:19 -0500

    Fibonacci_updated

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,16 @@
-def Fibonacci(x):
+def fib(N):
     ans = [0,1]
-    for i in range (1,x):
-        ans.append(ans[i-1] +ans[i]) 
-    return ans
-print(Fibonacci(13))
+    """
+    :type N: int
+    :rtype: int
+    """
+    if N == 0:
+        return 0
+    elif N == 1:
+        return 1
+    else:
+        for i in range (1,N):
+            ans.append(ans[i-1] +ans[i]) 
+    return ans[-1]
+
+print(fib(9))
